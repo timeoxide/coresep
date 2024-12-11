e2659fc8259a3abf8c5c877af3a167ea8d8b92f8
arash <arash.abdi.gh@gmail.com>
2024-12-11 19:34:30 +0330

    chore: add documentation comments to container.fromWindow and invoke functions

--- src/container.fromWindow.ts
+++ src/container.fromWindow.ts
@@ -1,5 +1,31 @@
 import { ICrsContainer } from "./abstractions";
 
+/**
+ * Retrieves the `crs` property attached to the global `window` object,
+ * if it was previously built with the `.AsWindowProp()` method in the
+ * `Factory`.
+ * 
+ * ---
+ * # **Example Usage:**
+ * ```typescript
+ * 
+ * Factory.new()
+ *      // register you modules here
+ *      .RegisterModule(<your module>)
+ *      // necessary for this function to work
+ *      .AsWindowProp()
+ *      // builds and adds container to window prop ( because `.AsWindowProp()` was called )
+ *      .Build()
+ * 
+ * ...
+ * const conatiner = container(); // this returns `window.crs`
+ * ...
+ * 
+ * ```
+ * 
+ * ---
+ * @returns The `ICrsContainer` instance if built with `.AsWindowProp()`, otherwise `undefined`.
+ */
 export function container(): ICrsContainer {
     return (window as any).crs;
 }
--- src/invoke.fromWindow.ts
+++ src/invoke.fromWindow.ts
@@ -1,5 +1,37 @@
 import { container } from "./container.fromWindow";
 
+/**
+ * Invokes a registered command in the commands container.
+ * it retrives the container from `container` function.
+ *
+ * ---
+ * 
+ * # **Example Usage:**
+ * ```typescript
+ * 
+ * Factory.new()
+ *      // register you modules here
+ *      .RegisterModule(<your module>)
+ *      // necessary for this function to work
+ *      .AsWindowProp()
+ *      // builds and adds container to window prop ( because `.AsWindowProp()` was called )
+ *      .Build()
+ * 
+ * ...
+ * const result = 
+ *      invoke<TypeOfYourModel, TypeOfYourResult>(
+ *          "myCommand", // the name of a registerd command
+ *          {} // the optional model to pass to your command
+ *      );
+ * ...
+ * 
+ * ```
+ * 
+ * ---
+ * @param name The name of the command to invoke.
+ * @param model Optional model to pass to the function.
+ * @returns The result of the invoked function.
+ */
 export async function invoke<TModel = any | undefined, TResult = any>(name: string, model?: TModel): Promise<TResult> {
     return await container().invoke(name, model);
 }
