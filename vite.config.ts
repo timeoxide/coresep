d65d320b17b7536f71401ef20694607947b97739
Arash Ghafoori <timeoxyde@gmail.com>
2025-04-16 06:33:05 +0330

    fix: fix issues related to import
    Fixed exports in package.json
    Changed the build proccess in order to generate cjs and es.

--- vite.config.ts
+++ vite.config.ts
@@ -1,49 +1,41 @@
-import { defineConfig, } from 'vite'
-import mkcert from 'vite-plugin-mkcert'
-import dts from 'vite-plugin-dts'
+import { defineConfig } from "vite";
+import mkcert from "vite-plugin-mkcert";
+import dts from "vite-plugin-dts";
 
 const is_development = true;
 export default defineConfig({
-
-    build: {
-        assetsDir: "assets",
-        target: "modules",
-        rollupOptions: {
-            input: ["src/index.ts"],
-            output: {
-                assetFileNames: "[name].[ext]",
-                entryFileNames: "[name].js"
-            }
-        },
-
-        sourcemap: true,
-        outDir: 'dist',
-        emptyOutDir: true,
-
-
-
-        lib: {
-            entry: "src/index.ts",
-            name: "coresep",
-            formats: ['es', 'cjs'],
-        },
+  build: {
+    lib: {
+      entry: "src/index.ts",
+      name: "coresep",
+      formats: ["es", "cjs"],
+      fileName: (format) => `index.${format === "cjs" ? "cjs" : "js"}`,
     },
-    server: {
-        host: true,
-        port: 2099
+    rollupOptions: {
+      output: {
+        entryFileNames: "[name].[format].js",
+      },
     },
-    resolve: {
-        alias: [{ find: '@', replacement: '/src' }],
-    },
-    plugins: [
-        mkcert(),
-        dts({
-            include: ["src/index.ts"],
-            // rollupTypes: true,
-            tsconfigPath: "./tsconfig.json",
-            outDir: "./dist",
-            insertTypesEntry: true,
 
-        })
-    ]
-})
+    sourcemap: true,
+    outDir: "dist",
+    emptyOutDir: true,
+  },
+  server: {
+    host: true,
+    port: 2099,
+  },
+  resolve: {
+    alias: [{ find: "@", replacement: "/src" }],
+  },
+  plugins: [
+    mkcert(),
+    dts({
+      include: ["src/index.ts"],
+      // rollupTypes: true,
+      tsconfigPath: "./tsconfig.json",
+      outDir: "./dist",
+      insertTypesEntry: true,
+    }),
+  ],
+});
